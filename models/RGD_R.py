e03413759b554c31ceeaabb5193500adbfa88f1d
subercui <subercui@gmail.com>
2019-12-18 01:05:53 -0500

    [update] temp commit

--- models/RGD_R.py
+++ models/RGD_R.py
@@ -1,67 +1,55 @@
 # -*- coding: utf-8 -*-
-# @Author       : William
-# @Project      : TextGAN-william
-# @FileName     : RelGAN_D.py
-# @Time         : Created at 2019-04-25
-# @Blog         : http://zhiweil.ml/
-# @Description  :
-# Copyrights (C) 2018. All Rights Reserved.
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from models.discriminator import CNNDiscriminator
-
-dis_filter_sizes = [2, 3, 4, 5]
-dis_num_filters = [300, 300, 300, 300]
+import spacy
 
 
-class RelGAN_D(CNNDiscriminator):
-    def __init__(self, embed_dim, max_seq_len, num_rep, vocab_size, padding_idx, gpu=False, dropout=0.25):
-        super(RelGAN_D, self).__init__(embed_dim, vocab_size, dis_filter_sizes, dis_num_filters, padding_idx,
-                                       gpu, dropout)
+class RGD_R(object):
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
 
-        self.embed_dim = embed_dim
-        self.max_seq_len = max_seq_len
-        self.feature_dim = sum(dis_num_filters)
-        self.emb_dim_single = int(embed_dim / num_rep)
+    def __call__(self, inp):
+        """wraps the input sentences into a list of structure nodes.
 
-        self.embeddings = nn.Linear(vocab_size, embed_dim, bias=False)
+        If  the inp is not string, first convert string using a dictionary.
 
-        self.convs = nn.ModuleList([
-            nn.Conv2d(1, n, (f, self.emb_dim_single), stride=(1, self.emb_dim_single)) for (n, f) in
-            zip(dis_num_filters, dis_filter_sizes)
-        ])
-
-        self.highway = nn.Linear(self.feature_dim, self.feature_dim)
-        self.feature2out = nn.Linear(self.feature_dim, 100)
-        self.out2logits = nn.Linear(100, 1)
-        self.dropout = nn.Dropout(dropout)
-
-        self.init_params()
-
-    def forward(self, inp):
+        Arguments:
+            inp {Optional[string, array]} -- [description]
         """
-        Get logits of discriminator
-        :param inp: batch_size * seq_len * vocab_size
-        :return logits: [batch_size * num_rep] (1-D tensor)
-        """
-        emb = self.embeddings(inp).unsqueeze(
-            1)  # batch_size * 1 * max_seq_len * embed_dim
+        text = self._convert(inp)
+        doc = self.nlp(text)
+        text_pile = []
+        dep_pile = []
+        for token in doc:
+            if token.dep_ == 'ROOT':
+                text_pile.append(token.text)
+                dep_pile.append(token.dep_)
+                for child in token.children:
+                    text_pile.append(child.text)
+                    dep_pile.append(child.dep_)
+        ('change here to some usual input format and types')
+        return text_pile, dep_pile
 
-        # [batch_size * num_filter * (seq_len-k_h+1) * num_rep]
-        cons = [F.relu(conv(emb)) for conv in self.convs]
-        pools = [F.max_pool2d(con, (con.size(2), 1)).squeeze(2)
-                 for con in cons]  # [batch_size * num_filter * num_rep]
-        pred = torch.cat(pools, 1)
-        # (batch_size * num_rep) * feature_dim
-        pred = pred.permute(0, 2, 1).contiguous().view(-1, self.feature_dim)
-        highway = self.highway(pred)
-        pred = torch.sigmoid(highway) * F.relu(highway) + \
-            (1. - torch.sigmoid(highway)) * pred  # highway
+    def _convert(self, inp):
+        if isinstance(inp, str):
+            return inp
+        elif isinstance(inp, torch.Tensor):
+            # a list of (one hot) arrays
+            raise NotImplementedError
 
-        pred = self.feature2out(self.dropout(pred))
-        logits = self.out2logits(pred).squeeze(1)  # [batch_size * num_rep]
 
-        return logits
+if __name__ == '__main__':
+    # parse a training document
+    doc_dir = '/home/haotian/Code/TextGAN-PyTorch/dataset/yelp.txt'
+    out_dir = '/home/haotian/Code/TextGAN-PyTorch/dataset/yelp_dep.txt'
+    read = RGD_R()
+    with open(doc_dir, 'r') as f:
+        with open(out_dir, 'w') as f_out:
+            while True:
+                line = f.readline()
+                _, dep_pile = read(line)
+                f_out.write(' '.join(dep_pile) + '\n')
+                if not line:
+                    break
