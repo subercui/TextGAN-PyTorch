8aa01747a848706974f9931c06a79d5341f7a7de
subercui <subercui@gmail.com>
2019-12-18 14:22:20 -0500

    format the RGD_G

--- models/RGD_G.py
+++ models/RGD_G.py
@@ -4,7 +4,7 @@
 # @FileName     : RelGAN_G.py
 # @Time         : Created at 2019-04-25
 # @Blog         : http://zhiweil.ml/
-# @Description  : 
+# @Description  :
 # Copyrights (C) 2018. All Rights Reserved.
 import torch
 import torch.nn as nn
@@ -18,13 +18,15 @@ from models.relational_rnn_general import RelationalMemory
 class RGD_G(LSTMGenerator):
     def __init__(self, mem_slots, num_heads, head_size, embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx,
                  gpu=False):
-        super(RGD_G, self).__init__(embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx, gpu)
+        super(RGD_G, self).__init__(embedding_dim, hidden_dim,
+                                    vocab_size, max_seq_len, padding_idx, gpu)
         self.name = 'relgan'
 
         self.temperature = 1.0  # init value is 1.0
 
         # RMC
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
+        self.embeddings = nn.Embedding(
+            vocab_size, embedding_dim, padding_idx=padding_idx)
         self.hidden_dim = mem_slots * num_heads * head_size
         self.lstm = RelationalMemory(mem_slots=mem_slots, head_size=head_size, input_size=embedding_dim,
                                      num_heads=num_heads, return_all_outputs=True)
@@ -37,6 +39,30 @@ class RGD_G(LSTMGenerator):
 
         self.init_params()
         pass
+
+    def forward(self, inp, hidden, need_hidden=False):
+        """
+        Embeds input and applies LSTM
+        :param inp: batch_size * seq_len
+        :param hidden: (h, c)
+        :param need_hidden: if return hidden, use for sampling
+        """
+        emb = self.embeddings(inp)  # batch_size * len * embedding_dim
+        if len(inp.size()) == 1:
+            emb = emb.unsqueeze(1)  # batch_size * 1 * embedding_dim
+
+        # out: batch_size * seq_len * hidden_dim  # also out is the pred, hidden is the memory
+        out, hidden = self.lstm(emb, hidden)
+        # out: (batch_size * len) * hidden_dim
+        out = out.contiguous().view(-1, self.hidden_dim)
+        out = self.lstm2out(out)  # (batch_size * seq_len) * vocab_size
+        out = self.temperature * out  # temperature
+        pred = self.softmax(out)
+
+        if need_hidden:
+            return pred, hidden
+        else:
+            return pred
 
     def step(self, inp, hidden):
         """
@@ -57,7 +83,8 @@ class RGD_G(LSTMGenerator):
         # next_token_onehot = F.one_hot(next_token, cfg.vocab_size).float()  # not used yet
         next_token_onehot = None
 
-        pred = F.softmax(gumbel_t * self.temperature, dim=-1)  # batch_size * vocab_size
+        pred = F.softmax(gumbel_t * self.temperature,
+                         dim=-1)  # batch_size * vocab_size
         # next_o = torch.sum(next_token_onehot * pred, dim=1)  # not used yet
         next_o = None
 
@@ -75,7 +102,8 @@ class RGD_G(LSTMGenerator):
         num_batch = num_samples // batch_size + 1 if num_samples != batch_size else 1
         samples = torch.zeros(num_batch * batch_size, self.max_seq_len).long()
         if one_hot:
-            all_preds = torch.zeros(batch_size, self.max_seq_len, self.vocab_size)
+            all_preds = torch.zeros(
+                batch_size, self.max_seq_len, self.vocab_size)
             if self.gpu:
                 all_preds = all_preds.cuda()
 
